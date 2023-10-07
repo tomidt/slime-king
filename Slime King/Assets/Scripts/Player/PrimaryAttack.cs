beccfade9a99047162873d8bef83625d20047d4a
tomidt <dtem0code@gmail.com>
2023-10-06 20:04:13 -1000

    Created primary attack

---
+++ Slime King/Assets/Scripts/Player/PrimaryAttack.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PrimaryAttack : MonoBehaviour
+{
+    [SerializeField] private InputActionReference primaryAttack;
+    [SerializeField] private FloatValue primaryAttackCooldown;
+
+    private bool canAttack = true;
+    private float currentCooldown = 0;
+
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+        if(!canAttack)
+        {
+            currentCooldown += Time.deltaTime;
+            if(currentCooldown >= primaryAttackCooldown.value)
+            {
+                canAttack = true;
+                currentCooldown = 0;
+            }
+        }
+
+        if(canAttack && primaryAttack.action.ReadValue<float>() != 0)
+        {
+            canAttack = false;
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attacked");
+    }
+
+
+    
+}
